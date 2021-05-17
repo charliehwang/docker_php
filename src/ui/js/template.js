25e313929e6cac773cd97b0cf9a7328a82af770c
charlie <charliehwang@gmail.com>
2021-05-17 07:07:11 -0600

    Added document ready

--- src/ui/js/template.js
+++ src/ui/js/template.js
@@ -1,3 +1,20 @@
-document.addEventListener('keyup', (e) => {
-  console.log(e.key)
+;(function (ready) {
+  if (
+    document.readyState === 'complete' ||
+    document.readyState === 'interactive'
+  ) {
+    ready()
+  } else {
+    document.addEventListener('readystatechange', function () {
+      if (document.readyState === 'complete') {
+        ready()
+      }
+    })
+  }
+})(() => {
+  /* the document is now complete. */
+
+  document.addEventListener('keyup', (e) => {
+    console.log(e.key)
+  })
 })
